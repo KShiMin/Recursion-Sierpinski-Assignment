d77b306b154ceb1c0573f18439fe30223fc356b8
KShiMin <KShiMin@github.com>
2022-01-23 20:26:27 +0800

    updated as of 23/01/2022 @ 20:26

--- Sierpinski_Circle.py
+++ Sierpinski_Circle.py
@@ -30,34 +30,40 @@ def sierpinski(point,radius,degree,skip,myTurtle):
         if nextSkip > 4:
             nextSkip = 1
 
-        # 1st circle (top left circle) NOT skipped
+        # 1st circle (top left circle) is skipped
+        # If skip = 2 / 3 / 4, start from bottom left
         if skip != 1:
-            # Starting point
+            # Start from bottom left
             sierpinski([point[0]-nextRadius, point[1]+radius-2*nextRadius], nextRadius, degree-1, nextSkip, myTurtle)
-            # sierpinski([point[0]+nextRadius, point[1]+radius-2*nextRadius], nextRadius, degree-1, nextSkip, myTurtle)
 
-        # 2nd circle (bottom left circle) NOT skipped
+        # 2nd circle (bottom left circle) is skipped
+        # If skip = 1 / 3 / 4, start from top left
         if skip != 2:
             # pass
             sierpinski([point[0]-nextRadius, point[1]+radius], nextRadius, degree-1, nextSkip, myTurtle)
         
-        # 3rd circle (top right circle) NOT skipped
+        # 3rd circle (top right circle) is skipped
         if skip != 3:
-            # pass
+            # Go to the right of the newly drawn circle
             sierpinski([point[0]+nextRadius, point[1]+radius], nextRadius, degree-1, nextSkip, myTurtle)
+            # sierpinski([point[0]+nextRadius, point[1]+radius-2*nextRadius], nextRadius, degree-1, nextSkip, myTurtle)
         
-        # 4th circle (bottom right circle) NOT skipped
+        # 4th circle (bottom right circle) is skipped
         if skip != 4:
-            # pass
+            # Go to the bottom of the newly drawn circke
+            # if degree is 1, it will move to the bottom after drawing the above code, however is degree changes, the pointer change
+            # Therefore, sometimes when drawn the shape does not go in seqeunce and jump around
             sierpinski([point[0]+nextRadius, point[1]+radius-2*nextRadius], nextRadius, degree-1, nextSkip, myTurtle)
+            # sierpinski([point[0]+nextRadius, point[1]+radius], nextRadius, degree-1, nextSkip, myTurtle)
+
 
 def main():
     myTurtle = turtle.Turtle()
     myTurtle.speed(2000)
     win = turtle.Screen()
     degree = 5
-    point = [0,-100]
-    radius = 200
+    point = [0,-250]
+    radius = 250
     skip = 1
 
     sierpinski(point, radius, degree, skip, myTurtle)
